6824b0bfe287e14ee481b8820782390143f65e87
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2024-12-01 19:59:00 +0200

    покращив vite, додав туди розбиття модулів для кращої продуктивності, і швидкості

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,45 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/goit-react-hw-05/", // Замініть на назву вашого репозиторію
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true, 
+    }),
+  ],
   build: {
-    outDir: "dist", // Директорія для зібраних файлів
-    emptyOutDir: true, // Очищує папку перед новою збіркою
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+      plugins: [visualizer()],
+    },
+    chunkSizeWarningLimit: 1000,
   },
 });
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   base: "/goit-react-hw-05/", // Замініть на назву вашого репозиторію
+//   plugins: [react()],
+//   build: {
+//     outDir: "dist", // Директорія для зібраних файлів
+//     emptyOutDir: true, // Очищує папку перед новою збіркою
+//   },
+// });
