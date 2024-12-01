4596cd2dfc3a6f2d24fa6c148dde1dd8fc77abd7
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2024-12-01 18:45:18 +0200

    ssss

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -44,7 +44,7 @@ export default function VideoPlayer({ filmId }) {
         .then((data) => {
           const iframeMatch = data.match(/<iframe.*<\/iframe>/gm);
           if (iframeMatch && iframeMatch.length > 0) {
-            setScriptHtml(iframeMatch[0]);
+            setScriptHtml(iframeMatch[1]);
           } else {
             console.error('IFrame not found in response');
           }
@@ -62,3 +62,33 @@ export default function VideoPlayer({ filmId }) {
       ></div>
     );
   }
+
+
+
+
+//   import classNames from 'classnames';
+// import React, { useEffect, useState } from 'react';
+
+// import styles from './VideoPlayer.module.css';
+
+// export default function VideoPlayer() {
+//   const [scriptHtml, setScriptHtml] = useState('');
+
+//   useEffect(() => {
+//     const dataUrl = window.location.href;
+//     fetch(
+//       '//pleer.videoplayers.club/get_player?w=610&h=370&type=widget&kp_id=&players=videocdn,hdvb,bazon,alloha,ustore,kodik,trailer,torrent&r_id=videoplayers&vni=VIDEOCDN&vti=&vdi=&hni=HDVB&hti=&hdi=&bni=BAZON&bti=&bdi=&alni=ALLOHATV&alti=&aldi=&usni=USTOREBZ&usti=&usdi=&koni=KODIK&koti=&kodi=&tti=&ru=' +
+//         dataUrl,
+//     )
+//       .then(res => res.text())
+//       .then(data => setScriptHtml(data.match(/<iframe.*<\/iframe>/gm)[1]));
+//   }, []);
+
+//   return (
+//     <div
+//       className={classNames('uitools', styles.video)}
+//       id="videoplayers"
+//       dangerouslySetInnerHTML={{ __html: scriptHtml }}
+//     ></div>
+//   );
+// }
